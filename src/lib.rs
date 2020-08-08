de686a39b9233cbd6a6e503e92ee43d004d800df
Spenser Black <spenserblack01@gmail.com>
2020-08-08 18:33:06 -0400

    Add docs for ++ and --

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
+//! `++` and `--` for Rust.
+//!
+//! # `++`
+//!
+//! ```
+//! # use plus_plus::ppmm;
+//! let mut x = 0;
+//!
+//! assert_eq!(ppmm!(x++), 0);
+//! assert_eq!(x, 1);
+//! assert_eq!(ppmm!(++x), 2);
+//! assert_eq!(x, 2);
+//! ```
+//!
+//! # `--`
+//!
+//! ```
+//! # use plus_plus::ppmm;
+//! let mut x = 10;
+//!
+//! assert_eq!(ppmm!(x--), 10);
+//! assert_eq!(x, 9);
+//! assert_eq!(ppmm!(--x), 8);
+//! assert_eq!(x, 8);
+//! ```
+
 #[cfg(test)]
 mod tests {
     #[test]
